89be25ae6106d64a0ec9b516d2b987e406482874
VladimirAus <v_roudakov@yahoo.com>
2015-05-06 19:52:50 +1000

    D3js: selectors

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 d3.select('.item').text('select');
-d3.selectAll('.item').text('select');
+d3.selectAll('#chart .item').text('selected');
+d3.selectAll('.item:nth-child(3)').text('select nth');
+d3.selectAll('.item:nth-child(odd)').text('select odd');
+d3.selectAll('.item:nth-child(even)').text('select even');
+d3.selectAll('.item:nth-child(n+3)').text('select even');
+d3.selectAll('.item:nth-child(2n)').text('select even');
