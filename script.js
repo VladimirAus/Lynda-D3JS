a015407c73789de1e06fe5b6f150ee9c6983a4c6
VladimirAus <v_roudakov@yahoo.com>
2015-05-08 01:43:48 +1000

    D3js: styles and classes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ d3.selectAll('.item:nth-child(even)').text('select even');
 d3.selectAll('.item:nth-child(n+3)').text('select 3');
 d3.selectAll('.item:nth-child(2n)').text('select 2n');
 */
-
+/*
 d3.selectAll('.item:nth-child(3n)')
 	.append('span')
 	.html(' <strong>Hippo</strong>');
@@ -16,3 +16,21 @@ d3.selectAll('#chart')
 	.html(' <strong>Panther</strong>');
 d3.select('.item:nth-child(3)')
 	.remove();
+*/
+d3.select('.item').
+	attr('class', 'highlight');
+d3.selectAll('.item:nth-child(3)').
+	classed('highlight', true);
+d3.selectAll('.item:nth-child(5)').
+	classed({
+		'highlight': true,
+		'item': false,
+		'bigger': true
+	});
+d3.selectAll('.item:nth-child(7)').
+	style({
+		'background': '#268BD2',
+		'color': '#EEE8D5',
+		'padding': '10px',
+		'margin': '5px'
+	});
